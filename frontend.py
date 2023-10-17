e3b927b92bcc58e4377cd2c28dabd68aa92b605e
hevardhan <hevardhan2004@gmail.com>
2023-10-17 15:36:12 +0530

    front-end commit

---
+++ frontend.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("https://images5.alphacoders.com/711/711093.jpg");
+            background-attachment: fixed;
+            background-size: cover
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+st.title("Brain Tumor Analysis")
