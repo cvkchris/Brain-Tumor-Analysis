a1c87f65d777d5bacc3d06a7dd774a037c2fbeba
hevardhan <hevardhan2004@gmail.com>
2023-10-18 16:14:24 +0530

    final changes

--- frontend.py
+++ frontend.py
@@ -4,7 +4,23 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.image import resize
 import tensorflow as tf
-from functions import *
+import cv2
+
+def equalize_image(image):
+    equalized = cv2.equalizeHist(image)
+    return cv2.cvtColor(equalized, cv2.COLOR_GRAY2BGR)
+
+def morph_ops(image, kernel):
+    # Apply binary threshold to the image
+    _, gray_image = cv2.threshold(image, 100, 255, cv2.THRESH_BINARY)
+
+    # Apply morphological operations on the binary image
+    closed_image = cv2.morphologyEx(gray_image, cv2.MORPH_CLOSE, kernel)
+    opened_image = cv2.morphologyEx(gray_image, cv2.MORPH_OPEN, kernel)
+    eroded_image = cv2.erode(gray_image, kernel, iterations=1)
+    dilated_image = cv2.dilate(gray_image, kernel, iterations=1)
+
+    return closed_image, opened_image, eroded_image, dilated_image
 
 # Load the saved model
 model = load_model('Brain_Tumor_cnn.h5')
