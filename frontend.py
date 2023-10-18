e315418d40d26d730810f6985d182d47eb663eb5
hevardhan <hevardhan2004@gmail.com>
2023-10-18 16:09:48 +0530

    added few function

--- frontend.py
+++ frontend.py
@@ -4,9 +4,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.image import resize
 import tensorflow as tf
-# import Morphological_Ops.ipynb as shit
-
-# shit.kirees()
+from functions import *
 
 # Load the saved model
 model = load_model('Brain_Tumor_cnn.h5')
@@ -48,18 +46,81 @@ st.markdown(
 st.title("Brain Tumor Classification")
 uploaded_image = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
+choice = st.sidebar.selectbox("Choose the Operation",('Predict Type','Histogram Equalization','Edge Detection','Morphological Operations','Spatial Filtering','Frequency Filtering'))
+
 if uploaded_image is not None:
-    try:
-        # Read the uploaded image
-        img = tf.image.decode_image(uploaded_image.read(), channels=3)
-        
+    img = tf.image.decode_image(uploaded_image.read(), channels=3)
+    img2 = cv2.cvtColor(img.numpy(), cv2.COLOR_BGR2GRAY)
+    if choice == 'Predict Type':
+        try:
+            st.subheader("Predict Type")
 
-        # Display the uploaded image
-        st.image(img.numpy(), caption = "Uploaded Image",width = 300)
-        
-        predicted_class = predict_class(img)
-        st.subheader("Result")
-        st.info(f"The predicted class for the image is: {predicted_class}")
+            # Display the uploaded image
+            st.image(img.numpy(), caption = "Uploaded Image",width = 300)
+            
+            predicted_class = predict_class(img)
+            st.subheader("Result")
+            st.info(f"The predicted class for the image is: {predicted_class}")
 
-    except Exception as e:
-        st.text(f"An error occurred: {e}")
+        except Exception as e:
+            st.text(f"An error occurred: {e}")
+
+    elif choice == 'Histogram Equalization':
+        st.subheader("Histogram Equalization")
+        equalized_image = equalize_image(img2)
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(img.numpy(),caption = "Original Image",width = 300)
+        with col2:
+            st.image(equalized_image,caption = "Equalized Image",width = 300)
+    
+    elif choice == 'Morphological Operations':
+        kernel = np.ones((7,7), np.uint8)
+        c_im,o_im,e_im,d_im = morph_ops(img2,kernel)
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(c_im,caption = "Image After Closing",width = 300)
+            st.image(e_im,caption = "Image After Erosion",width = 300)
+        with col2:
+            st.image(o_im,caption = "Image After Opening",width = 300)
+            st.image(d_im,caption = "Image After Dilaiton",width = 300)
+        
+else:
+    st.header("About")
+
+    # Introduction
+    st.write("This application is designed to help you analyze brain tumor images. It provides various image processing techniques and information about the creator.")
+
+    # Types of Brain Tumors
+    st.subheader("Types of Brain Tumors")
+    st.write("There are several types of brain tumors, including:")
+    st.write("- Glioma Tumor")
+    st.write("- No Tumor (Normal Brain)")
+    st.write("- Pituitary Tumor")
+    st.write("- Meningioma Tumor")
+
+    st.header("Operations this app supports")
+    # Edge Detection
+    st.subheader("Edge Detection")
+    st.write("Edge detection is a technique used to identify the boundaries of objects within an image. It helps in highlighting the edges of structures in brain tumor images, making it easier to analyze them.")
+
+    # Histogram Equalization
+    st.subheader("Histogram Equalization")
+    st.write("Histogram equalization is a method used to improve the contrast of an image by redistributing the intensity levels. It can enhance the visibility of features in brain tumor images.")
+
+    # Morphological Operations
+    st.subheader("Morphological Operations")
+    st.write("Morphological operations involve the use of structuring elements to process binary images. These operations, such as dilation and erosion, can help in enhancing or removing certain features in the image.")
+
+    # Creator's Name
+    st.header("Creator Info")
+    st.write("This Brain Tumor Analysis App was created by : ")
+    st.write("- Chris Vinod Kurian")
+    st.write("- Drishtti Narwal")
+    st.write("- Gaurav Prakash")
+    st.write("- Hevardhan Saravanan")
+
+    # Disclaimer
+    st.warning("This application is intended for educational and informational purposes. It is not a substitute for professional medical advice. Consult a medical professional for accurate diagnosis and treatment of brain tumors.")
+
+    
